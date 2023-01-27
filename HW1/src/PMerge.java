c8817e570e3a5ba6d4842048aeb8862c8875b7dc
Keshav Narasimhan <keshavnarasimhan123@gmail.com>
2023-01-26 23:38:40 -0600

    ascending parallel merge algorithm, need to make it descending

--- HW1/src/PMerge.java
+++ HW1/src/PMerge.java
@@ -11,5 +11,93 @@ public class PMerge {
      */
     public static void parallelMerge(int[] A, int[] B, int[] C, int numThreads) {
         // TODO: Implement your parallel merge function
+    	if (C.length < A.length + B.length) {
+    		System.out.println("Ensure that C is large enough!");
+    		return;
+    	}
+    	
+    	ExecutorService es = Executors.newFixedThreadPool(numThreads);
+    	for (int i = 0; i < A.length; i++) {
+    		es.submit(new MergeHelper(B, C, i, A[i], true));
+    	}
+    	for (int i = 0; i < B.length; i++) {
+    		es.submit(new MergeHelper(A, C, i, B[i], false));
+    	}
+    	
+    	es.shutdown();
+    	while(!es.isTerminated());
+//    	System.out.println(Arrays.toString(C));
     }
+    
+//    public static void main(String[] args) {
+//    	int[] A = {1, 2, 4, 5};
+//    	int[] B = {0, 7};
+//    	int[] C = new int[6];
+//    	parallelMerge(A, B, C, 5);
+//    }
+}
+
+class MergeHelper implements Callable<Integer> {
+	int[] A;
+	int[] C;
+	int ind;
+	int target;
+	boolean findMaxPos;
+	
+	public MergeHelper(int[] A, int[] C, int index, int target, boolean b) {
+		this.A = A;
+		this.C = C;
+		this.ind = index;
+		this.target = target;
+		this.findMaxPos = b;
+	}
+	
+	public Integer call() {
+		int newIndex = ind + getLocation();
+		C[newIndex] = this.target;
+		return 0;
+	}
+	
+	public int getLocation() {
+		int low = 0;
+		int high = A.length - 1;
+		int index = (low + high) / 2;
+		int pindex = -1;
+		while (index >= 0 && index < A.length) {
+			if (A[index] == this.target) {
+				if(this.findMaxPos) {
+					index += 1;
+					while(index < A.length && A[index] == target) {
+						index += 1;
+					}
+//					System.out.println("target = " + target + ", " + Arrays.toString(this.A) + ", index = " + index);
+					return index;
+				} else {
+					index -= 1;
+					while(index >= 0 && A[index] == target) {
+						index -= 1;
+					}
+//					System.out.println("target = " + target + ", " + Arrays.toString(this.A) + ", index = " + index);
+					return index + 1;
+				}
+			} else if (pindex == index) {
+				return A[index] > this.target ? index : index + 1;
+			} else if (A[index] < this.target) {
+				low = index + 1;
+				pindex = index;
+				index = (low + high) / 2;
+			} else {
+				high = index - 1;
+				pindex = index;
+				index = (low + high) / 2;
+			}
+		}
+		
+		return index;
+	}
+	
+//	public static void main(String[] args) {
+//		int[] A = {1};
+//		System.out.println(new MergeHelper().getLocation(A, 3));
+//	}
 }
